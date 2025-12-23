58c341e1544f451dabc3f1dfee402f5c27545499
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 20:49:40 +0300

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+  return(
+    <nav className="nav">
+      <a href="/">LIST</a>
+      <a href="/leaderboard">LEADERBOARD</a>
+      <a href="/submit">SUBMIT</a>
+      <a href="/admin">ADMIN</a>
+    </nav>
+  )
+}
