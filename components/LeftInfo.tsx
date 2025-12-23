ac4d3e4ea1306cd7072102ae8dddf5c26e18274d
DARKPINGGD <omerasarfino@gmail.com>
2025-12-23 20:49:57 +0300

    Create LeftInfo.tsx

---
+++ components/LeftInfo.tsx
@@ -0,0 +1,13 @@
+export default function LeftInfo(){
+  return(
+    <div className="left">
+      <p><b>Main List</b><br/>#1 – #75</p>
+      <p><b>Extended List</b><br/>#76 – #150</p>
+      <p><b>Legacy List</b><br/>#151+</p>
+
+      <a className="discord" href="https://discord.gg/YOURSERVER">
+        DISCORD
+      </a>
+    </div>
+  )
+}
